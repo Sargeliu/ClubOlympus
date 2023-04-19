b7d25ec39406390bd0acff2a249236c14324b894
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-04-19 15:45:52 +0500

    Отображение данных

--- app/src/main/java/com/example/clubolympus/MainActivity.java
+++ app/src/main/java/com/example/clubolympus/MainActivity.java
@@ -5,19 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import com.example.clubolympus.data.ClubOlympusContract.*;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView dataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataTextView = findViewById(R.id.dataTextView);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +37,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayData();
+    }
 
+    private void displayData() {
+        String[] projection = {
+            MemberEntry._ID,
+            MemberEntry.COLUMN_FIRST_NAME,
+            MemberEntry.COLUMN_GENDER,
+            MemberEntry.COLUMN_SPORT
+        };
+
+        Cursor cursor = getContentResolver().query(
+                MemberEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
+
+        dataTextView.setText("All members\n\n");
+        dataTextView.append(
+                MemberEntry._ID + " " +
+                MemberEntry.COLUMN_FIRST_NAME + " " +
+                MemberEntry.COLUMN_GENDER + " " +
+                MemberEntry.COLUMN_SPORT
+        );
+
+        int idColumnIndex = cursor.getColumnIndex(MemberEntry._ID);
+        int FirstNameColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_FIRST_NAME);
+        int LastNameColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_LAST_NAME);
+        int GenderColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_GENDER);
+        int SportColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_SPORT);
+
+        while (cursor.moveToNext()) {
+            int currentId = cursor.getInt(idColumnIndex);
+            String currentFirstName = cursor.getString(FirstNameColumnIndex);
+            String currentLastName = cursor.getString(LastNameColumnIndex);
+            int currentGender = cursor.getInt(GenderColumnIndex);
+            String currentSport = cursor.getString(SportColumnIndex);
+
+            dataTextView.append("\n" +
+                    currentId + " " +
+                    currentFirstName + " " +
+                    currentLastName + " " +
+                    currentGender + " " +
+                    currentSport);
+        }
+        cursor.close();
+    }
 }
